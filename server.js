4e5d79a8a753d2b45f2b46f170abb8928a5ce5b1
Cory Holenstein <cholensteinwork@gmail.com>
2022-03-07 16:31:30 -0500

    register end point

--- server.js
+++ server.js
@@ -26,68 +26,6 @@ app.use((req, res, next) => {
 
 
 
-app.post('/users/register', (req, res) => {
-    const email = req.email;
-    const password = req.password;
-    const password_confirmation = req.password_confirmation;
-
-
-    console.log(email);
-
-    pool.getConnection(function (err, connection) {
-        console.log("Connected!");
-
-        /* if (!email || !password || !password_confirmation) {
-             res.send({ errormessage: "Please fill in all the fields" });
-             return;
-         }*/
-
-        //Check passwords match
-        if (password != password_confirmation) {
-            console.log('Passwords dont match');
-            res.send({ errormessage: "Passwords dont match" });
-            return;
-        }
-
-        //Check password length
-        if (password.length < 6) {
-            res.send({ errormessage: 'Password should be atleast 6 characters' });
-            return;
-
-        }
-
-        connection.query("SELECT email FROM user WHERE email = ?", [email], (err, result) => {
-            if (err) throw err;
-
-
-            //You will get an array. if no users found it will return.
-
-            if (result.length > 0) {
-                console.log("we found a user???");
-                console.log(result);
-                res.send({ errormessage: "Email already exists!" });
-
-                return;
-            } else {
-                connection.query("INSERT INTO user (email, password) VALUES (?,?)", [email, password], (err, result) => {
-                    if (err) {
-                        res.send({ err: err });
-                        return;
-                    }
-                    res.send(email);
-                });
-                console.log("inserted new user!");
-                return;
-            }
-
-        });
-        connection.release();
-
-
-    });
-
-
-});
 
 
 
@@ -247,6 +185,75 @@ app.post("/users/login", (req, res) => {
     });
 
 });
+
+app.post('/users/register', (req, res) => {
+    const username = req.body.username;
+    const password = req.bodypassword;
+    const password_confirmation = req.body.passwordConfirmation;
+
+
+ 
+
+    pool.getConnection(function (err, connection) {
+        console.log("Connected!");
+
+        /* if (!email || !password || !password_confirmation) {
+             res.send({ errormessage: "Please fill in all the fields" });
+             return;
+         }*/
+
+        //Check passwords match
+        //Check password length
+       /* if (password.length < 6) {
+            res.send({ errormessage: "Password should be atleast 6 characters" });
+            return;
+
+        }
+        if (password != password_confirmation) {
+            console.log("Passwords dont match");
+            res.send({ errormessage: "Passwords dont match" });
+            return;
+        }
+        */
+     
+
+        connection.query("SELECT username FROM users WHERE username = ?", [username], (err, result) => {
+            console.log(result);
+            if (err) {
+                res.send({ err: err });
+
+            }
+
+
+            //You will get an array. if no users found it will return.
+
+            if (result.length > 0) {
+                console.log("we found a user???");
+                console.log(result);
+                res.send({ errormessage: "Email already exists!" });
+
+                
+            } else {
+                connection.query("INSERT INTO users (username, password) VALUES (?,?)", [username, password], (err, result) => {
+                    if (err) {
+                        res.send({ err: err });
+                       
+                    }
+                    res.send(username);
+                });
+                console.log("inserted new user!");
+                connection.release();
+            }
+
+        });
+     //   connection.release();
+
+
+    });
+
+
+});
+
 
 app.get('/', function (req, res) {
     console.log("Connected!");
